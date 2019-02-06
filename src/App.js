ee5306d9428db05310168890eea18de8d61c53de
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-02-06 08:17:03 +0300

    setting up semantic ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './img/logo.png';
+import { Container } from 'semantic-ui-react';
+import { Input } from 'semantic-ui-react'
+import { Button, Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
+
 
 class App extends Component {
+  state = { activeItem: 'انشاء الشبكة' }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
+    const { activeItem } = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+          <Menu attached="top" stackable pointing >
+            <Menu.Item>
+              <Input className='icon' icon='search' placeholder='Search...' />
+            </Menu.Item>
+            <Menu.Item
+              name="المستشفيات"
+              active={activeItem === 'المستشفيات'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name="الصيدليات"
+              active={activeItem === 'الصيدليات'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name="انشاء الشبكة"
+              active={activeItem === 'انشاء الشبكة'}
+              onClick={this.handleItemClick}
+              position="left"
+            />
+
+            <Menu.Item position="right">
+               <p className="header-name">وزارة الصحة السعودية</p>
+            </Menu.Item>
+            <Menu.Item >
+              <img src={logo} className="logo" alt="logo" />
+            </Menu.Item>
+          </Menu>
+
+          <Container style={{paddingTop: 20}}>
+            <Container>
+              <Segment>
+                Medicl Records
+              </Segment>
+            </Container>
+
+            <Container>
+              <Segment>
+                Medicl Records
+              </Segment>
+            </Container>
+
+            <Container>
+              <Segment>
+                Medicl Records
+              </Segment>
+            </Container>
+          </Container>
+
       </div>
     );
   }
