f246e9585c4273e78d5e97d1505a20868e4fd35a
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-02-09 05:21:21 +0300

    rendering the hosptials inside the table

--- src/components/Hospitals.js
+++ src/components/Hospitals.js
@@ -5,12 +5,44 @@ import logo from '../img/logo.png';
 
 
 export default class Hospitals extends Component {
+  renderTable = () => {
+    const { hospitals } = this.props;
+
+    for (let i = 0; i < hospitals.length; i++) {
+      return (
+        <Table.Row>
+          <Table.Cell>{i+1}</Table.Cell>
+          <Table.Cell>{hospitals[i].name}</Table.Cell>
+          <Table.Cell>{hospitals[i].networkAddress}</Table.Cell>
+          <Table.Cell>September 14, 2013</Table.Cell>
+        </Table.Row> 
+      );
+    }
+
+    hospitals
+  }
+
   render() { 
     const { hospitals } = this.props;
+
+    let tableBody;
+    if (hospitals != null) {
+      tableBody = hospitals.map((hospital, index) => {
+        return (
+        <Table.Row key={index}>
+          <Table.Cell>{index+1}</Table.Cell>
+          <Table.Cell>{hospital.name}</Table.Cell>
+          <Table.Cell>{hospital.networkAddress}</Table.Cell>
+          <Table.Cell>September 14, 2013</Table.Cell>
+        </Table.Row> 
+        );
+      });
+    }
+
+
     return (
       <div>
         <h1 className="menu-title">Hospitals</h1>
-
         <Segment padded>
           <Table>
             <Table.Header>
@@ -22,21 +54,7 @@ export default class Hospitals extends Component {
               </Table.Row>
             </Table.Header>
             <Table.Body>
-              <Table.Row>
-                <Table.Cell>John Lilki</Table.Cell>
-                <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
-                <Table.Cell>September 14, 2013</Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>Jamie Harington</Table.Cell>
-                <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
-                <Table.Cell>January 11, 2014</Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>Jill Lewis</Table.Cell>
-                <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
-                <Table.Cell>May 11, 2014</Table.Cell>
-              </Table.Row>
+              {tableBody}
             </Table.Body>
           </Table>
         </Segment>
